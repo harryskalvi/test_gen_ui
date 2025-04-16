ec68bb3e369d3729cc7144961dbbc7b3d52bebdc
harryskalvi <harryskalvi@gmail.com>
2025-04-16 13:05:53 +0530

    test servo

--- test_hkr_logic.py
+++ test_hkr_logic.py
@@ -1,4 +1,9 @@
 import random
+from adafruit_servokit import ServoKit
+import sys
+
+# Initialize PCA9685 with 16 channels
+kit = ServoKit(channels=16)
 
 def generateRandomPunches():
     leftPunches = ["jab", "LHook", "LUpperCut"]
@@ -10,10 +15,14 @@ def beginSparring():
     print("Sparring session started.")
     punches = generateRandomPunches()
     print(punches)
+    kit.servo[4].angle = 45
+    kit.servo[4].angle = 135
 
 def beginCombination(combination, loop=False):
     print("Combination session started : ", combination)
     print("Loop ", loop)
+    kit.servo[0].angle = 90
+    kit.servo[0].angle = 45
 
 def endWorkout():
     print("Workout session ended.") # This is a placeholder for the actual end workout logic.
